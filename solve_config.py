47ca514db4f7d6a4e4480ed013f138c684c96cbb
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2024-03-22 11:51:59 +0100

    Set default move size back to 1

--- solve_config.py
+++ solve_config.py
@@ -3,7 +3,7 @@ from multiprocessing import cpu_count
 
 class SolveConfig:
     def __init__(self):
-        self.move_size = 4
+        self.move_size = 1
         self.use_sat_solver = False
         self.symmetric_move_ban_depth = 0
         self.max_solver_threads = cpu_count() - 1
