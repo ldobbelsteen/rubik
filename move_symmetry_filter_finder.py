f9d4699cec4a37b43683da1158b2aa1f0cc7ca68
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2024-03-26 18:14:53 +0100

    Add lt and st

--- move_symmetry_filter_finder.py
+++ move_symmetry_filter_finder.py
@@ -16,14 +16,25 @@ class VarConditions:
         self.d = d
 
         # Declare Z3 variables for each variant.
-        # TODO: add smaller than, less than operators
-        self.const_equalities = [z3.Bool(f"{name}(s + {s}) == {v}") for v in domain]
-        self.const_inequalities = [z3.Bool(f"{name}(s + {s}) != {v}") for v in domain]
-        self.symb_equalities = [
+        self.const_eq = [z3.Bool(f"{name}(s + {s}) == {v}") for v in domain]
+        self.const_ineq = [z3.Bool(f"{name}(s + {s}) != {v}") for v in domain]
+        self.symb_eq = [
             z3.Bool(f"{name}(s + {s}) == {name}(s + {f})") for f in range(s + 1, d)
         ]
-        self.symb_inequalities = [
+        self.symb_ineq = [
             z3.Bool(f"{name}(s + {s}) != {name}(s + {f})") for f in range(s + 1, d)
+        ]
+        self.symb_lt = [
+            z3.Bool(f"{name}(s + {s}) > {name}(s + {f})") for f in range(s + 1, d)
+        ]
+        self.symb_st = [
+            z3.Bool(f"{name}(s + {s}) < {name}(s + {f})") for f in range(s + 1, d)
+        ]
+        self.symb_lte = [
+            z3.Bool(f"{name}(s + {s}) ≥ {name}(s + {f})") for f in range(s + 1, d)
+        ]
+        self.symb_ste = [
+            z3.Bool(f"{name}(s + {s}) ≤ {name}(s + {f})") for f in range(s + 1, d)
         ]
 
     def facilitates(
@@ -33,23 +44,23 @@ class VarConditions:
         next_conditions: list["VarConditions"],
     ):
         assert len(next_values) == (self.d - self.s - 1) == len(next_conditions)
-        domain_idx = self.domain.index(v)
+        idx = self.domain.index(v)
         conflicting = []
 
         # Disallow equality to different constants.
-        for i, const_eq in enumerate(self.const_equalities):
-            if i != domain_idx:
+        for i, const_eq in enumerate(self.const_eq):
+            if i != idx:
                 conflicting.append(const_eq)
 
         # Disallow inequality to the value.
-        conflicting.append(self.const_inequalities[domain_idx])
+        conflicting.append(self.const_ineq[idx])
 
         # Disallow symbolic equality to a next condition that does not
         # facilitate or that has a different value.
-        for s in range(len(self.symb_equalities)):
+        for s in range(len(self.symb_eq)):
             conflicting.append(
                 z3.And(
-                    self.symb_equalities[s],
+                    self.symb_eq[s],
                     z3.Or(
                         v != next_values[s],
                         z3.Not(
@@ -65,10 +76,10 @@ class VarConditions:
 
         # Disallow symbolic inequality to a next condition that facilitates
         # or that has the same value.
-        for s in range(len(self.symb_inequalities)):
+        for s in range(len(self.symb_ineq)):
             conflicting.append(
                 z3.And(
-                    self.symb_inequalities[s],
+                    self.symb_ineq[s],
                     z3.Or(
                         v == next_values[s],
                         next_conditions[s].facilitates(
@@ -80,20 +91,128 @@ class VarConditions:
                 )
             )
 
+        # Disallow larger than comparison with a next condition that facilitates
+        # no larger values or has smaller than or equal value.
+        for s in range(len(self.symb_lt)):
+            conflicting.append(
+                z3.And(
+                    self.symb_lt[s],
+                    z3.Or(
+                        v <= next_values[s],
+                        z3.Not(
+                            z3.Or(
+                                [
+                                    next_conditions[s].facilitates(
+                                        nv,
+                                        next_values[s + 1 :],
+                                        next_conditions[s + 1 :],
+                                    )
+                                    for nv in self.domain[idx + 1 :]
+                                ]
+                            )
+                        ),
+                    ),
+                )
+            )
+
+        # Disallow smaller than comparison with a next condition that facilitates
+        # no smaller values or has larger than or equal value.
+        for s in range(len(self.symb_st)):
+            conflicting.append(
+                z3.And(
+                    self.symb_st[s],
+                    z3.Or(
+                        v >= next_values[s],
+                        z3.Not(
+                            z3.Or(
+                                [
+                                    next_conditions[s].facilitates(
+                                        nv,
+                                        next_values[s + 1 :],
+                                        next_conditions[s + 1 :],
+                                    )
+                                    for nv in self.domain[:idx]
+                                ]
+                            )
+                        ),
+                    ),
+                )
+            )
+
+        # Disallow larger than or equal comparison with a next condition that
+        # facilitates no larger than or equal values or has smaller value.
+        for s in range(len(self.symb_lte)):
+            conflicting.append(
+                z3.And(
+                    self.symb_lte[s],
+                    z3.Or(
+                        v < next_values[s],
+                        z3.Not(
+                            z3.Or(
+                                [
+                                    next_conditions[s].facilitates(
+                                        nv,
+                                        next_values[s + 1 :],
+                                        next_conditions[s + 1 :],
+                                    )
+                                    for nv in self.domain[idx:]
+                                ]
+                            )
+                        ),
+                    ),
+                )
+            )
+
+        # Disallow smaller than or equal comparison with a next condition that
+        # facilitates no smaller than or equal values or has larger value.
+        for s in range(len(self.symb_ste)):
+            conflicting.append(
+                z3.And(
+                    self.symb_ste[s],
+                    z3.Or(
+                        v > next_values[s],
+                        z3.Not(
+                            z3.Or(
+                                [
+                                    next_conditions[s].facilitates(
+                                        nv,
+                                        next_values[s + 1 :],
+                                        next_conditions[s + 1 :],
+                                    )
+                                    for nv in self.domain[: idx + 1]
+                                ]
+                            )
+                        ),
+                    ),
+                )
+            )
+
         return z3.Not(z3.Or(conflicting))
 
     def extract_from_model(self, model: z3.ModelRef) -> list[z3.BoolRef]:
         result = []
-        for v in self.const_equalities:
+        for v in self.const_eq:
             if z3.is_true(model.get_interp(v)):
                 result.append(v)
-        for v in self.const_inequalities:
+        for v in self.const_ineq:
             if z3.is_true(model.get_interp(v)):
                 result.append(v)
-        for v in self.symb_equalities:
+        for v in self.symb_eq:
             if z3.is_true(model.get_interp(v)):
                 result.append(v)
-        for v in self.symb_inequalities:
+        for v in self.symb_ineq:
+            if z3.is_true(model.get_interp(v)):
+                result.append(v)
+        for v in self.symb_lt:
+            if z3.is_true(model.get_interp(v)):
+                result.append(v)
+        for v in self.symb_st:
+            if z3.is_true(model.get_interp(v)):
+                result.append(v)
+        for v in self.symb_lte:
+            if z3.is_true(model.get_interp(v)):
+                result.append(v)
+        for v in self.symb_ste:
             if z3.is_true(model.get_interp(v)):
                 result.append(v)
         return result
@@ -160,42 +279,78 @@ def find(n: int, d: int):
         [
             z3.If(v, 1, 0)
             for c in chain(ax_conditions, hi_conditions, dr_conditions)
-            for v in c.const_equalities
-            + c.const_inequalities
-            + c.symb_equalities
-            + c.symb_inequalities
+            for v in c.const_eq
+            + c.const_ineq
+            + c.symb_eq
+            + c.symb_ineq
+            + c.symb_lt
+            + c.symb_st
+            + c.symb_lte
+            + c.symb_ste
+        ]
+    )
+    symb_lte_count = z3.Sum(
+        [
+            z3.If(v, 1, 0)
+            for c in chain(ax_conditions, hi_conditions, dr_conditions)
+            for v in c.symb_lte
+        ]
+    )
+    symb_ste_count = z3.Sum(
+        [
+            z3.If(v, 1, 0)
+            for c in chain(ax_conditions, hi_conditions, dr_conditions)
+            for v in c.symb_ste
+        ]
+    )
+    symb_lt_count = z3.Sum(
+        [
+            z3.If(v, 1, 0)
+            for c in chain(ax_conditions, hi_conditions, dr_conditions)
+            for v in c.symb_lt
+        ]
+    )
+    symb_st_count = z3.Sum(
+        [
+            z3.If(v, 1, 0)
+            for c in chain(ax_conditions, hi_conditions, dr_conditions)
+            for v in c.symb_st
         ]
     )
     symb_ineq_count = z3.Sum(
         [
             z3.If(v, 1, 0)
             for c in chain(ax_conditions, hi_conditions, dr_conditions)
-            for v in c.symb_inequalities
+            for v in c.symb_ineq
         ]
     )
     symb_eq_count = z3.Sum(
         [
             z3.If(v, 1, 0)
             for c in chain(ax_conditions, hi_conditions, dr_conditions)
-            for v in c.symb_equalities
+            for v in c.symb_eq
         ]
     )
     const_ineq_count = z3.Sum(
         [
             z3.If(v, 1, 0)
             for c in chain(ax_conditions, hi_conditions, dr_conditions)
-            for v in c.const_inequalities
+            for v in c.const_ineq
         ]
     )
     const_eq_count = z3.Sum(
         [
             z3.If(v, 1, 0)
             for c in chain(ax_conditions, hi_conditions, dr_conditions)
-            for v in c.const_equalities
+            for v in c.const_eq
         ]
     )
     solver.maximize(filtered_count)
     solver.minimize(condition_count)
+    solver.minimize(symb_lte_count)
+    solver.minimize(symb_ste_count)
+    solver.minimize(symb_lt_count)
+    solver.minimize(symb_st_count)
     solver.minimize(symb_ineq_count)
     solver.minimize(symb_eq_count)
     solver.minimize(const_ineq_count)
