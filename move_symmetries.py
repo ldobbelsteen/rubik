a65d857d3efede43e502384e9ce0a944ee55c59a
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2024-03-28 16:00:50 +0100

    Disable existing filters

--- move_symmetries.py
+++ move_symmetries.py
@@ -45,6 +45,8 @@ def allowed_by_filters(n: int, seq: MoveSeq) -> bool:
     def drs(s: int):
         return seq[s][2]
 
+    return True
+
     # Symmetric move filter #1
     for s in range(k - 1):
         if axs(s) == axs(s + 1) and his(s) and not his(s + 1):
