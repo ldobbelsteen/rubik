158109d3dc77b101f973889505be85e4147674d4
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2024-04-03 12:55:36 +0200

    Output results immediately

--- benchmark.py
+++ benchmark.py
@@ -3,8 +3,6 @@
 import os
 import signal
 
-import pandas as pd
-
 from puzzle import Puzzle
 from solve import solve
 from solve_config import SolveConfig
@@ -34,103 +32,112 @@ def raise_(ex):
 
 def benchmark_move_sizes(config: SolveConfig, move_sizes: list[int]):
     """Benchmark the solve function for a list of move sizes."""
-    signal.signal(
-        signal.SIGALRM,
-        lambda signum, frame: raise_(Exception(f"timeout {signum} {frame}")),
-    )
-
     print_stamped("benchmarking move sizes...")
-    results = []
-    for puzzle in load_benchmark_puzzles():
-        print_stamped(f"puzzle {puzzle.name}...")
-        fastest: float | None = None
-
-        for move_size in move_sizes:
-            print_stamped(f"move size {move_size}...")
-            config.move_size = move_size
-
-            if fastest is not None:
-                signal.alarm(int(fastest * 3))
-
-            try:
-                stats = solve(puzzle, config, False)
-            except Exception as e:
-                print_stamped(f"exception occurred: {e}")
-                results.append((puzzle.name, move_size, -1, -1, -1))
-            else:
-                signal.alarm(0)
-                results.append(
-                    (
-                        puzzle.name,
-                        move_size,
-                        stats.total_prep_time().total_seconds(),
-                        stats.total_solve_time().total_seconds(),
-                        stats.k(),
-                    )
-                )
-                total = (
-                    stats.total_prep_time().total_seconds()
-                    + stats.total_solve_time().total_seconds()
-                )
-                if fastest is None or total < fastest:
-                    fastest = total
-
-    pd.DataFrame(
-        data=results,
-        columns=["puzzle_name", "move_size", "prep_time", "solve_time", "k"],
-    ).to_csv(os.path.join(BENCHMARK_DIR, "move_sizes.csv"), index=False)
-
-
-def benchmark_thread_count(config: SolveConfig, thread_counts: list[int]):
-    """Benchmark the solve function for a list of thread counts."""
     signal.signal(
         signal.SIGALRM,
         lambda signum, frame: raise_(Exception(f"timeout {signum} {frame}")),
     )
 
-    print_stamped("benchmarking thread counts...")
-    results = []
-    for puzzle in load_benchmark_puzzles():
-        print_stamped(f"puzzle {puzzle.name}...")
-        fastest: float | None = None
+    path = os.path.join(BENCHMARK_DIR, "move_sizes.csv")
+    if not os.path.exists(path):
+        with open(path, "w") as f:
+            f.write("puzzle_name,move_size,prep_time,solve_time,k\n")
 
-        for thread_count in thread_counts:
-            print_stamped(f"thread count {thread_count}...")
-            config.max_solver_threads = thread_count
+    with open(path, "a") as f:
+        for puzzle in load_benchmark_puzzles():
+            print_stamped(f"puzzle {puzzle.name}...")
+            fastest: float | None = None
 
-            if fastest is not None:
-                signal.alarm(int(fastest * 3))
+            for move_size in move_sizes:
+                print_stamped(f"move size {move_size}...")
+                config.move_size = move_size
 
-            try:
-                stats = solve(puzzle, config, False)
-            except Exception as e:
-                print_stamped(f"exception occurred: {e}")
-                results.append((puzzle.name, thread_count, -1, -1, -1))
-            else:
-                signal.alarm(0)
-                results.append(
-                    (
-                        puzzle.name,
-                        thread_count,
-                        stats.total_prep_time().total_seconds(),
-                        stats.total_solve_time().total_seconds(),
-                        stats.k(),
+                if fastest is not None:
+                    signal.alarm(int(fastest * 3))
+
+                try:
+                    stats = solve(puzzle, config, False)
+                except Exception as e:
+                    print_stamped(f"exception occurred: {e}")
+                    f.write(
+                        ",".join([puzzle.name, str(move_size), "-1", "-1", "-1"]) + "\n"
                     )
-                )
-                total = (
-                    stats.total_prep_time().total_seconds()
-                    + stats.total_solve_time().total_seconds()
-                )
-                if fastest is None or total < fastest:
-                    fastest = total
+                else:
+                    signal.alarm(0)
+                    f.write(
+                        ",".join(
+                            [
+                                puzzle.name,
+                                str(move_size),
+                                str(stats.total_prep_time().total_seconds()),
+                                str(stats.total_solve_time().total_seconds()),
+                                str(stats.k()),
+                            ]
+                        )
+                    )
+                    total = (
+                        stats.total_prep_time().total_seconds()
+                        + stats.total_solve_time().total_seconds()
+                    )
+                    if fastest is None or total < fastest:
+                        fastest = total
 
-    pd.DataFrame(
-        data=results,
-        columns=["puzzle_name", "thread_count", "prep_time", "solve_time", "k"],
-    ).to_csv(os.path.join(BENCHMARK_DIR, "thread_counts.csv"), index=False)
+
+def benchmark_thread_counts(config: SolveConfig, thread_counts: list[int]):
+    """Benchmark the solve function for a list of thread counts."""
+    print_stamped("benchmarking thread counts...")
+    signal.signal(
+        signal.SIGALRM,
+        lambda signum, frame: raise_(Exception(f"timeout {signum} {frame}")),
+    )
+
+    path = os.path.join(BENCHMARK_DIR, "thread_counts.csv")
+    if not os.path.exists(path):
+        with open(path, "w") as f:
+            f.write("puzzle_name,thread_count,prep_time,solve_time,k\n")
+
+    with open(path, "a") as f:
+        for puzzle in load_benchmark_puzzles():
+            print_stamped(f"puzzle {puzzle.name}...")
+            fastest: float | None = None
+
+            for thread_count in thread_counts:
+                print_stamped(f"thread count {thread_count}...")
+                config.max_solver_threads = thread_count
+
+                if fastest is not None:
+                    signal.alarm(int(fastest * 3))
+
+                try:
+                    stats = solve(puzzle, config, False)
+                except Exception as e:
+                    print_stamped(f"exception occurred: {e}")
+                    f.write(
+                        ",".join([puzzle.name, str(thread_count), "-1", "-1", "-1"])
+                        + "\n"
+                    )
+                else:
+                    signal.alarm(0)
+                    f.write(
+                        ",".join(
+                            [
+                                puzzle.name,
+                                str(thread_count),
+                                str(stats.total_prep_time().total_seconds()),
+                                str(stats.total_solve_time().total_seconds()),
+                                str(stats.k()),
+                            ]
+                        )
+                    )
+                    total = (
+                        stats.total_prep_time().total_seconds()
+                        + stats.total_solve_time().total_seconds()
+                    )
+                    if fastest is None or total < fastest:
+                        fastest = total
 
 
 if __name__ == "__main__":
     os.makedirs(BENCHMARK_DIR, exist_ok=True)
-    benchmark_thread_count(SolveConfig.default(), [1, 2, 4, 7])
+    benchmark_thread_counts(SolveConfig.default(), [1, 2, 4, 7])
     benchmark_move_sizes(SolveConfig.default(), [1, 2, 3, 4])
